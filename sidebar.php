4a68ae242c78b187b41532bf2ccdbbb6e4a549d2
marhowlett <marimar_r2003@hotmail.com>
2019-08-14 11:09:05 -0500

    .

---
+++ sidebar.php
@@ -0,0 +1,77 @@
+ <section id="aviso" class="template-section">
+ [user-data field_name="Logotipo"]
+ [user-data field_name="Nombre de la agencia"]
+ [bt_bb_tour_search_form label_keyword="" label_destination="" label_departure_date="" label_price_from="" label_price_to="" label_search_button="" show_keyword="" show_departure_date="" show_prices_filters="" show_categories="" responsive="" publish_datetime="" expiry_datetime="" el_id="" el_class="" el_style=""][/bt_bb_tour_search_form]
+</section>
+
+
+<section class="app" id="app">
+  <aside class="sidebar">
+         
+    <nav class="sidebar-nav">
+ 
+      <ul>
+       <li class="dos">
+          <a href="#"><span class="">Programación Regular</span></a>
+          <a href="#"><span class="">Salidas especiales</span></a>
+          <a href="#"><span class="">Superbuscador</span></a>
+        </li>
+        <li >
+          <span>Kit de herramientas</span>
+          <ul class="nav-flyout">
+            <li>
+              <a href="#">Material sin logo</a>
+            </li>
+            <li>
+              <a href="#">Webinars</a>
+            </li>
+            <li>
+              <a href="#">FAM TRIPS</a>
+            </li>
+           
+          </ul>
+        </li>
+        <li class="dos">
+          <span>Mis Reservas</span>
+          <ul class="nav-flyout">
+            <li>
+              <a href="#">Central de reservas</a>
+            </li>
+            <li>
+              <a href="#">Corporativo Vivatours</a>
+            </li>
+            
+           
+          </ul>
+        </li>
+        <li>
+          <span>Información general</span>
+          <ul class="nav-flyout">
+            <li>
+              <a href="#">Beneficios </a>
+            </li>
+            <li>
+              <a href="#">Condiciones de colaboración</a>
+            </li>
+            <li>
+              <a href="#">Descuentos y regalos</a>
+            </li>
+            <li>
+              <a href="#">Nuestros servicios </a>
+            </li>
+           
+          </ul>
+        </li>
+       
+      
+        <li class="dos">
+          <a href="#"><span class="">Nuestros operadores</span></a>
+        </li>
+        <li>
+          <a href="#"><span class="">Contacto</span></a>
+          <a href="#"><span class="">Ayuda</span></a>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+</section>
