894dd65c7ee7f64a9eeafbdcf2c3ac7eb4561a3f
marhowlett <marimar_r2003@hotmail.com>
2019-11-25 15:01:21 -0600

    .

--- travelicious/amp/templates/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="amp-wp-footer">
-	<div>
-		<h2><?php echo esc_html( wptexturize( $this->get( 'blog_name' ) ) ); ?></h2>
-		<a href="#top" class="back-to-top"><?php esc_html_e( 'Back to top', 'bt_plugin' ); ?></a>
-	</div>
-</footer>
