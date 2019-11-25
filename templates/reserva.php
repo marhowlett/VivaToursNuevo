c843dea6c9ea4428c7696bdf1d08e871f418d311
marhowlett <marimar_r2003@hotmail.com>
2019-11-25 16:51:24 -0600

    .

---
+++ templates/reserva.php
@@ -0,0 +1,95 @@
+<table width="865" border="0" align="center" cellpadding="5" cellspacing="0" rules="none" style="font-family:Arial, Helvetica, sans-serif; border-collapse:collapse;">
+  <tr bgcolor="#009999" >
+    <th colspan="4" scope="col"><p style="font-size:18px; color:#FFF;">Gracias por reservar con:</p>
+    <p><a href="https://viajesvivatours.com"><img src="https://www.viajesvivatours.com/wp-content/uploads/2017/10/logoViva1-1.png" width="100" /></a></p></th>
+    <th width="548" scope="col"><font style="font-size:18px; color:#FFF; ">Este bloqueo es  preventivo. </font><br />
+    <font style="font-size:18px; color:#ffff00; ">Cuentas con 72 horas para 
+    realizar<br />
+el pago</font><font style="font-size:18px; color:#FFF;"> de esta reserva para<br />garantizar tus espacios y salida.  </font></th>
+  </tr>
+  <tr>
+    <td colspan="5" align="left" valign="middle">
+    
+   <!-- <font style="color:#F00;">No. Bloqueo:'.$compras.'</font><br>-->
+    <br>
+   <strong> Nombre:</strong>%nombre% <br />
+	<strong>Correo:</strong>%correo_electronico% <br />
+	<strong>Teléfono:</strong>%tel% <br />
+	<strong>Programa:</strong>%programa% <br />
+	<strong>No. Habitaciones:</strong>%no_de_habitaciones% <br>
+	<strong>Comentarios:</strong> %comentarios%
+	</td>
+  </tr>
+  <tr>
+    <td colspan="5" align="center" valign="middle">Aquí puedes consultar todos los detalles de tu reservación, cualquier duda consultanos.</td>
+  </tr>
+  <tr>
+    <td colspan="5">'.$tr.'</td>
+  </tr>
+  <tr>
+    <td colspan="5"  ><span style="color:#FFF;">Datos Bancarios:</span></td>
+  </tr>
+  <tr>
+    <td colspan="5" bgcolor="#009999" style="color:#FFF;" >PARA DEPOSITOS EN PESOS (Pedir tipo de cambio del día) .</td>
+  </tr>
+  <tr>
+    <td colspan="5">
+    <p>Viva Tours Agencia Mayorista de Viajes SA de CV  <br>
+      Cuenta Nº: 03902426153  <br>
+      Suc: 006  <br>
+      Plaza: 039 </p>
+      <p>CLABE 044540039024261532  <br>
+      SCOTIABANK INVERLAT SA  <br>
+      DOMICILIO: CUERNAVACA, MORELOS. MEXICO<br>
+Viva Tours Agencia Mayorista de Viajes SA de CV  <br>
+Cuenta Nº: 16926  <br>
+Suc: 997  <br>
+CLABE 002540099700169260  <br>
+BANAMEX, SA  <br>
+DOMICILIO: CUERNAVACA, MORELOS. MEXICO  <br>
+<br>
+Viva Tours Agencia Mayorista de Viajes SA de CV<br>
+Cuenta Nº: 0176917232  <br>
+Suc: 0817  <br>
+CLABE 012540001769172328  <br>
+BBVA BANCOMER SA  <br>
+DOMICILIO: CUERNAVACA, MORELOS. MEXICO</p>
+      </td></tr><tr><td colspan="5" bgcolor="#009999" style="color:#FFF;" >
+      <p>PARA DEPOSITOS EN DOLARES:   </td></tr><tr><td colspan="5">
+        Viva Tours Agencia Mayorista de Viajes SA de CV  <br>
+        Cuenta Nº: 03900002771  <br>
+        Suc: 006  <br>
+        Plaza: 039<br>
+        <br>
+        CLABE 044540039000027716  <br>
+        SCOTIABANK INVERLAT SA  <br>
+        SWIFT CODE: MBCOMXMM  <br>
+        Av. Vicente Guerrero No. 110<br>
+        Col. Lomas de la Selva CP62270<br>
+        Cuernavaca. Morelos. MEXICO  <br>
+        <br>
+        Viva Tours Agencia Mayorista de Viajes SA de CV<br>
+        Cuenta Nº: 9000208  <br>
+        Suc: 997 <br>
+        CLABE 002540099790002081  <br>
+        BANAMEX, SA  <br>
+        DOMICILIO: CUERNAVACA, MORELOS. MEXICO  <br>
+        <br>
+        Viva Tours Agencia Mayorista de Viajes SA de CV  <br>
+        Cuenta Nº: 0176922287  <br>
+        Suc: 0817<br>
+        CLABE PARA TRANSFERENCIA: 012540001769222870  <br>
+        BBVA BANCOMER SA  <br>
+        CODIGO SWIFT: BCMRMXMM  <br>
+        DOMICILIO: CUERNAVACA, MORELOS. MEXICO  <br>
+        </td></tr><tr><td colspan="5" bgcolor="#009999" style="color:#FFF;" >
+        PARA TRANSFERNECIA EN EUROS: </td></tr><tr><td colspan="5">
+        <br>
+        Viva Tours Agencia Mayorista de Viajes SA de CV  <br>
+        Cuenta Nº: 0176922562  Suc: 0817  <br>
+        CLABE 012540001769225628  <br>
+        BBVA BANCOMER SA  <br>
+        CODIGO SWIFT: BCMRMXMM  <br>
+    DOMICILIO: CUERNAVACA, MORELOS. MEXICO</p></td>
+  </tr>
+</table>
