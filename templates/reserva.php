89b18cb4932a3c2881c89ef419897070d617b89b
marhowlett <marimar_r2003@hotmail.com>
2019-12-12 17:25:54 -0600

    .

--- templates/reserva.php
+++ templates/reserva.php
@@ -94,3 +94,103 @@ DOMICILIO: CUERNAVACA, MORELOS. MEXICO</p>
             DOMICILIO: CUERNAVACA, MORELOS. MEXICO</td>
   </tr>
 </table>
+
+
+
+
+<table style="font-family: Arial, Helvetica, sans-serif; border-collapse: collapse; width: 83.7407%; height: 1675px;" border="0" width="865" rules="none" cellspacing="0" cellpadding="5" align="center">
+<tbody>
+<tr style="height: 133px;">
+<th style="height: 133px;" colspan="4" scope="col">
+<p style="font-size: 18px; color: #fff;"><span style="font-family: arial, helvetica, sans-serif;"><span style="color: #000000;">Gracias por reservar con:</span></span></p>
+<p style="font-size: 18px; color: #fff;"><span style="font-family: arial, helvetica, sans-serif;">
+<a href="https://viajesvivatours.com"><img class="aligncenter" src="https://www.viajesvivatours.com/wp-content/uploads/2017/10/logoViva1-1.png" alt="" width="170" height="72" /></a></span></p>
+</th>
+</tr>
+<tr bgcolor="#009999">
+<th style="text-align: center;"><span style="font-size: 18px; font-family: arial, helvetica, sans-serif;"><span style="color: #ffffff;">Este bloqueo es preventivo.</span>  </span><span style="font-family: arial, helvetica, sans-serif;"><span style="font-size: 18px; color: #ffff00;">Cuentas con 72 horas para
+realizar el pago</span><span style="font-size: 18px; color: #ffffff;"> de esta reserva para garantizar tus espacios y salida. </span></span></th>
+</tr>
+<tr style="height: 174px;">
+<td style="height: 174px;" colspan="5" align="left" valign="middle"><span style="font-family: arial, helvetica, sans-serif;"><!-- <font style="color:#F00;">No. Bloqueo:'.$compras.'</font>
+--></span><span style="font-family: arial, helvetica, sans-serif;"><strong> Nombre:</strong>%nombre%</span>
+<span style="font-family: arial, helvetica, sans-serif;"><strong>Correo:</strong>%correo_electronico%</span>
+<span style="font-family: arial, helvetica, sans-serif;"><strong>Teléfono:</strong>%tel%</span>
+<span style="font-family: arial, helvetica, sans-serif;"><strong>Programa:</strong>%programa%</span>
+<span style="font-family: arial, helvetica, sans-serif;"><strong>No. Habitaciones:</strong>%no_de_habitaciones%</span>
+<span style="font-family: arial, helvetica, sans-serif;"><strong>Comentarios:</strong> %comentarios%</span></td>
+</tr>
+<tr style="height: 22px;">
+<td style="height: 22px;" colspan="5" align="center" valign="middle"><span style="font-family: arial, helvetica, sans-serif;">Puedes consultar tus solicitudes de reservación en tu panel de agentes en la opción de Mis reservas.</span></td>
+</tr>
+<tr style="height: 22px;">
+<td style="height: 22px;" colspan="5">
+<h3><span style="font-family: arial, helvetica, sans-serif;">Datos Bancarios:</span></h3>
+</td>
+</tr>
+<tr style="height: 22px;">
+<td style="color: #ffffff; height: 22px;" colspan="5" bgcolor="#009999">
+<h4><strong><span style="font-family: arial, helvetica, sans-serif;">PARA DEPOSITOS EN PESOS (Pedir tipo de cambio del día) .:</span></strong></h4>
+</td>
+</tr>
+<tr style="height: 142px;">
+<td style="height: 142px;" colspan="5"><span style="font-family: arial, helvetica, sans-serif;">Viva Tours Agencia Mayorista de Viajes SA de CV</span>
+<span style="font-family: arial, helvetica, sans-serif;">Cuenta Nº: 03902426153</span>
+<span style="font-family: arial, helvetica, sans-serif;">Suc: 006</span>
+<span style="font-family: arial, helvetica, sans-serif;">Plaza: 039</span><span style="font-family: arial, helvetica, sans-serif;">CLABE 044540039024261532</span>
+<span style="font-family: arial, helvetica, sans-serif;">SCOTIABANK INVERLAT SA</span>
+<span style="font-family: arial, helvetica, sans-serif;">DOMICILIO: CUERNAVACA, MORELOS. MEXICO</span>
+<span style="font-family: arial, helvetica, sans-serif;">Viva Tours Agencia Mayorista de Viajes SA de CV</span>
+<span style="font-family: arial, helvetica, sans-serif;">Cuenta Nº: 16926</span>
+<span style="font-family: arial, helvetica, sans-serif;">Suc: 997</span>
+<span style="font-family: arial, helvetica, sans-serif;">CLABE 002540099700169260</span>
+<span style="font-family: arial, helvetica, sans-serif;">BANAMEX, SA</span>
+<span style="font-family: arial, helvetica, sans-serif;">DOMICILIO: CUERNAVACA, MORELOS. MEXICO</span><span style="font-family: arial, helvetica, sans-serif;">Viva Tours Agencia Mayorista de Viajes SA de CV</span>
+<span style="font-family: arial, helvetica, sans-serif;">Cuenta Nº: 0176917232</span>
+<span style="font-family: arial, helvetica, sans-serif;">Suc: 0817</span>
+<span style="font-family: arial, helvetica, sans-serif;">CLABE 012540001769172328</span>
+<span style="font-family: arial, helvetica, sans-serif;">BBVA BANCOMER SA</span>
+<span style="font-family: arial, helvetica, sans-serif;">DOMICILIO: CUERNAVACA, MORELOS. MEXICO</span></td>
+</tr>
+<tr style="height: 22px;">
+<td style="color: #ffffff; height: 22px;" colspan="5" bgcolor="#009999">
+<h4><strong><span style="font-family: arial, helvetica, sans-serif;">PARA DEPOSITOS EN DOLARES::</span></strong></h4>
+</td>
+</tr>
+<tr style="height: 142px;">
+<td style="height: 142px;" colspan="5"><span style="font-family: arial, helvetica, sans-serif;">Viva Tours Agencia Mayorista de Viajes SA de CV</span>
+<span style="font-family: arial, helvetica, sans-serif;">Cuenta Nº: 03900002771</span>
+<span style="font-family: arial, helvetica, sans-serif;">Suc: 006</span>
+<span style="font-family: arial, helvetica, sans-serif;">Plaza: 039</span><span style="font-family: arial, helvetica, sans-serif;">CLABE 044540039000027716</span>
+<span style="font-family: arial, helvetica, sans-serif;">SCOTIABANK INVERLAT SA</span>
+<span style="font-family: arial, helvetica, sans-serif;">SWIFT CODE: MBCOMXMM</span>
+<span style="font-family: arial, helvetica, sans-serif;">Av. Vicente Guerrero No. 110</span>
+<span style="font-family: arial, helvetica, sans-serif;">Col. Lomas de la Selva CP62270</span>
+<span style="font-family: arial, helvetica, sans-serif;">Cuernavaca. Morelos. MEXICO</span><span style="font-family: arial, helvetica, sans-serif;">Viva Tours Agencia Mayorista de Viajes SA de CV</span>
+<span style="font-family: arial, helvetica, sans-serif;">Cuenta Nº: 9000208</span>
+<span style="font-family: arial, helvetica, sans-serif;">Suc: 997</span>
+<span style="font-family: arial, helvetica, sans-serif;">CLABE 002540099790002081</span>
+<span style="font-family: arial, helvetica, sans-serif;">BANAMEX, SA</span>
+<span style="font-family: arial, helvetica, sans-serif;">DOMICILIO: CUERNAVACA, MORELOS. MEXICO</span><span style="font-family: arial, helvetica, sans-serif;">Viva Tours Agencia Mayorista de Viajes SA de CV</span>
+<span style="font-family: arial, helvetica, sans-serif;">Cuenta Nº: 0176922287</span>
+<span style="font-family: arial, helvetica, sans-serif;">Suc: 0817</span>
+<span style="font-family: arial, helvetica, sans-serif;">CLABE PARA TRANSFERENCIA: 012540001769222870</span>
+<span style="font-family: arial, helvetica, sans-serif;">BBVA BANCOMER SA</span>
+<span style="font-family: arial, helvetica, sans-serif;">CODIGO SWIFT: BCMRMXMM</span>
+<span style="font-family: arial, helvetica, sans-serif;">DOMICILIO: CUERNAVACA, MORELOS. MEXICO</span></td>
+</tr>
+<tr style="height: 22px;">
+<td style="color: #ffffff; height: 22px;" colspan="5" bgcolor="#009999">
+<h4><strong><span style="font-family: arial, helvetica, sans-serif;">PARA TRANSFERNECIA EN EUROS:</span></strong></h4>
+</td>
+</tr>
+<tr style="height: 142px;">
+<td style="height: 142px;" colspan="5"><span style="font-family: arial, helvetica, sans-serif;">Viva Tours Agencia Mayorista de Viajes SA de CV</span>
+<span style="font-family: arial, helvetica, sans-serif;">Cuenta Nº: 0176922562 Suc: 0817</span>
+<span style="font-family: arial, helvetica, sans-serif;">CLABE 012540001769225628</span>
+<span style="font-family: arial, helvetica, sans-serif;">BBVA BANCOMER SA</span>
+<span style="font-family: arial, helvetica, sans-serif;">CODIGO SWIFT: BCMRMXMM</span>
+<span style="font-family: arial, helvetica, sans-serif;">DOMICILIO: CUERNAVACA, MORELOS. MEXICO</span></td>
+</tr>
+</tbody>
+</table>
